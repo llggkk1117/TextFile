436df583a21121fff40fffabd9f55bc7f002484c
Gene Lee <gene.kwan.lee@gmail.com>
2015-08-17 18:06:58 -0700

    initial commit
    initial commit

---
+++ src/org/gene/textFile/charset/ByteRange.java
@@ -0,0 +1,42 @@
+package org.gene.textFile.charset;
+
+public class ByteRange
+{
+	private int numOfBytes;
+	private byte[] startingByte;
+	private byte[] endingByte;
+
+	public ByteRange(){}
+
+	public ByteRange(String startingByte, String endingByte)
+	{
+		this.numOfBytes = startingByte.length()/2;
+		this.startingByte = HexStringBinay.h2b(startingByte);
+		this.endingByte = HexStringBinay.h2b(endingByte);
+	}
+
+	public int getNumOfBytes()
+	{
+		return this.numOfBytes;
+	}
+
+	public byte[] getStartingByte()
+	{
+		return this.startingByte;
+	}
+
+	public String getStartingByteHexString()
+	{
+		return HexStringBinay.b2h(this.startingByte);
+	}
+
+	public byte[] getEndingByte()
+	{
+		return this.endingByte;
+	}
+
+	public String getEndingByteHexString()
+	{
+		return HexStringBinay.b2h(this.endingByte);
+	}
+}
